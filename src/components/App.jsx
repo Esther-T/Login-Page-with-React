f94cd3ac6ad04e60afa85fa42d1af0fe491c9420
Esther-T <tan.esther8@gmail.com>
2021-01-07 21:56:18 -0800

    Added Input component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,11 @@ import Login from "./Login.jsx";
 var isLoggedIn = false;
 
 function App() {
-  if (isLoggedIn === true) return <h1>Hello</h1>;
-  else return <Login />;
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1>;
+  } else {
+    return <Login />;
+  }
 }
 
 export default App;
