1f2950632023397a121ca0ed77c668c2ba62b61c
Esther-T <tan.esther8@gmail.com>
2021-01-07 22:17:03 -0800

    Worked on ternary operations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,11 @@ import Login from "./Login.jsx";
 var isLoggedIn = false;
 
 function App() {
-  if (isLoggedIn === true) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
+  return (
+    <div className="container">
+      {isLoggedIn === true ? <h1>Hello</h1> : <Login />}
+    </div>
+  );
 }
 
 export default App;
