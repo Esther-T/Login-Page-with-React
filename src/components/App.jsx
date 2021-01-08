b9754765b2a7268a2f733efeaa263c08ae5738f8
Esther-T <tan.esther8@gmail.com>
2021-01-07 22:23:59 -0800

    Worked on ternary operations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Login from "./Login.jsx";
 
 var isLoggedIn = false;
-
+var currentTime = new Date().getHours();
+console.log(currentTime);
 function App() {
   return (
     <div className="container">
-      {isLoggedIn === true ? <h1>Hello</h1> : <Login />}
+      {isLoggedIn ? <h1>Hello</h1> : <Login />}
+      {currentTime > 24 ? <h3>Why are you still working?</h3> : null}
     </div>
   );
 }
