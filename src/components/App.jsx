4dd5e844a0c88244c59eae22f9578002930debd5
Esther-T <tan.esther8@gmail.com>
2021-01-08 19:48:37 -0800

    Cleaned up code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,14 @@ import Login from "./Login.jsx";
 import Form from "./Form.jsx";
 
 var isLoggedIn = false;
-var userIsRegistered = false;
+var userIsRegistered = true;
 
 var currentTime = new Date().getHours();
 console.log(currentTime);
 function App() {
   return (
     <div className="container">
-      {userIsRegistered ? <Form /> : isLoggedIn ? <h1>Hello</h1> : <Login />}
+      {!userIsRegistered ? <Form /> : isLoggedIn ? <h1>Hello</h1> : <Login />}
     </div>
   );
 }
