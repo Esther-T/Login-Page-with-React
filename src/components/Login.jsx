998a4f65587ef604de9e9891be9f169db67f1ec9
Esther-T <tan.esther8@gmail.com>
2021-01-10 18:52:08 -0800

    Worked on Event Handling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,11 +2,28 @@ import React from "react";
 import Input from "./Input.jsx";
 
 function Login() {
+  const [isMousedOver, setMouseOver] = React.useState(false);
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
   return (
     <form className="form">
       <Input type="text" placeholder="username" />
       <Input type="password" placeholder="password" />
-      <button type="submit">Login</button>
+      <button
+        type="submit"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+      >
+        Login
+      </button>
     </form>
   );
 }
