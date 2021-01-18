dd3fbf7aa46f7bbf5d39d495cb4c302c13d33013
Esther-T <tan.esther8@gmail.com>
2021-01-17 22:48:47 -0800

    Fixed password bug

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 function Form() {
   function checkPassword(event) {
-    event.target.value.length < 5
+    event.target.value.length <= 5
       ? (document.getElementById("passMessage").innerHTML =
           "Password needs to be greater than 5 characters!")
       : (document.getElementById("passMessage").innerHTML = "");
   }
 
   function confirmPassword(event) {
-    const pass = document.getElementById("password").value;
-    event.target.value.length !== pass
+    const pass = document.getElementById("originalPassword").value;
+    event.target.value !== pass
       ? (document.getElementById("passMessage").innerHTML =
           "Password not the same!")
       : (document.getElementById("passMessage").innerHTML = "");
@@ -22,13 +22,14 @@ function Form() {
       <font id="passMessage" color="red"></font>
       <input
         onChange={checkPassword}
+        id="originalPassword"
         type="password"
         placeholder="Password"
         required
       />
       <input
         type="password"
-        id="password"
+        id="confirmPassword"
         onChange={confirmPassword}
         placeholder="Confirm Password"
         required
